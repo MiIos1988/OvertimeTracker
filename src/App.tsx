d7c4b7cedda0afdf88df14426a4b4a84552cb455
Milos1988 <vojvoda1988@gmail.com>
2024-02-03 01:16:24 +0100

    Add aws jwt token

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Amplify } from "aws-amplify";
+import { fetchAuthSession } from "aws-amplify/auth";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import amplifyConfig from "./amplifyconfiguration.json";
@@ -7,7 +8,24 @@ import MainComponent from "./components/MainComponent";
 Amplify.configure(amplifyConfig);
 
 function App() {
-  const [jwtToken, setJwtToken] = useState(null);
+  const [jwtToken, setJwtToken] = useState("");
+
+  useEffect(() => {
+    async function fetchToken() {
+      try {
+        const authToken = (
+          await fetchAuthSession()
+        ).tokens?.idToken?.toString();
+        if (authToken) {
+          setJwtToken(authToken);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchToken();
+  }, []);
   return (
     <Authenticator>
       {({ signOut, user }) => (
